817e36d2ff326b57fbf39110c7d01ef286728025
Qobil <102223264+Qobi1@users.noreply.github.com>
2023-08-08 00:01:28 +0500

    Update settings.py

--- superkonkursbot/settings.py
+++ superkonkursbot/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'superkonkursbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "railway",
+        'USER': "postgres",
+        'PASSWORD': 'DEddDv4GIT8LECW7EKAz',
+        'HOST': 'containers-us-west-93.railway.app',
+        'PORT': '6195',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
